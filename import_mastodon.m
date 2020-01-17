eef55fd6fed51f76f5ae3f619ed0c4ecf9abe522
Jean-Yves Tinevez <tinevez@pasteur.fr>
2020-01-17 19:30:28 +0100

    Import tags and concatenate them to the graph tables.

--- import_mastodon.m
+++ import_mastodon.m
@@ -1,4 +1,4 @@
-function graph = import_mastodon( source_file )
+function [ G, tss ] = import_mastodon( source_file )
 
     % Uncompress data file into temp dir.
     filenames = unzip( source_file, fullfile( tempdir, source_file ) );
@@ -6,8 +6,46 @@ function graph = import_mastodon( source_file )
     % Find the uncompressed file for the graph -> model.raw.
     mastodon_model_file = get_model_file( filenames );
     % Deserialize it.
-    graph = import_mastodon_graph( mastodon_model_file );
+    G = import_mastodon_graph( mastodon_model_file );
     
+    % Find the uncompressed file for the tags -> tags.raw.
+    mastodon_tag_file = get_tag_file( filenames );
+    % Deserialize it.
+   [ tag_table_vertices, tag_table_edges, tss ]  = import_mastodon_tags( mastodon_tag_file );
+   
+   % Merge model table and tag table.
+   n_tag_set = numel( tss );
+   
+   if ~isempty( tag_table_vertices )       
+       for i = 1 : n_tag_set
+           
+           tag_set_name = tss( i ).name;
+           
+           col = categorical();
+           col( numnodes( G ), 1 ) = '<undefined>';
+           
+           [ ~, idx ] = ismember( tag_table_vertices.id, G.Nodes.id );
+           col( idx ) = tag_table_vertices.( tag_set_name );
+           G.Nodes.( tag_set_name ) = col;
+           
+       end
+   end
+   
+   if ~isempty( tag_table_edges )       
+       for i = 1 : n_tag_set
+           
+           tag_set_name = tss( i ).name;
+           
+           col = categorical();
+           col( numedges( G ), 1 ) = '<undefined>';
+           
+           [ ~, idx ] = ismember( tag_table_edges.id, G.Nodes.id );
+           col( idx ) = tag_table_edges.( tag_set_name );
+           G.Edges.( tag_set_name ) = col;
+           
+       end
+   end
+   
     
     %% Functions.
     
@@ -20,6 +58,17 @@ function graph = import_mastodon( source_file )
         end
         
         model_file = filenames{ id };
+    end
+
+    function tag_file = get_tag_file( filenames )
+        
+        id = find( ~cellfun( @isempty, regexp( filenames, '.*tags.raw$') ) );
+        if isempty( id )
+            error( 'MastodonImporter:missingTagFile', ...
+                'Could not find tag file in Mastodon file.' )
+        end
+        
+        tag_file = filenames{ id };
     end
 
 end
